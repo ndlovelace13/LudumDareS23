131609c3606e4ae0e7ac9dc1b6d3b15ffead18c3
Noah Lovelace <ndlovelace13@gmail.com>
2023-04-29 14:46:18 -0700

    Revert "Merge pull request #28 from ndlovelace13/AIWork"
    This reverts commit a091e6922f7c34ba0758811a4576bfec565ddc94, reversing
    changes made to c1d4a8cf84c7cdf33e3305825aa693859177e619.

--- LudumDareS23/Assets/Scripts/DrivingBehavior.cs
+++ LudumDareS23/Assets/Scripts/DrivingBehavior.cs
@@ -7,19 +7,24 @@ public class DrivingBehavior : MonoBehaviour
 {
     [SerializeField]PathCreator pathCreator;
     [SerializeField] float speed = 5;
-    [SerializeField] float startingLocation;
     float distanceTravelled;
+    //[SerializeField] int direction;
+    Quaternion directionFaced;
     // Start is called before the first frame update
     void Start()
     {
-        distanceTravelled = startingLocation;
+        //gameObject.transform.Rotate(0, 0, 90f, Space.Self);
     }
 
     // Update is called once per frame
     void Update()
     {
-        distanceTravelled += speed * Time.deltaTime;
+        distanceTravelled += speed * Time.deltaTime; //* direction;
         transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
         transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+        /*if (direction < 0)
+            transform.rotation = Quaternion.Inverse(directionFaced);
+        else
+            transform.rotation = directionFaced;*/
     }
 }
