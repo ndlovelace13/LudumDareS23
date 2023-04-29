0188a23d0c1df216e23f33e6fa7f6302798297b3
Madeline Mayans <82738387+mmayans@users.noreply.github.com>
2023-04-28 22:16:18 -0700

    added the pepperonis and all of that jazz

--- LudumDareS23/Assets/Scripts/pizzaActivation.cs
+++ LudumDareS23/Assets/Scripts/pizzaActivation.cs
@@ -9,6 +9,7 @@ public class pizzaActivation : MonoBehaviour
     public GameObject pizza;
     public double score;
     public int target;
+    public double bonus;
 
     public GameObject arrow;
     public GameObject house1;
@@ -16,6 +17,11 @@ public class pizzaActivation : MonoBehaviour
     public GameObject house3;
     public GameObject house4;
     public GameObject house5;
+
+    public GameObject pep1;
+    public GameObject pep2;
+    public GameObject pep3;
+    public GameObject pep4;
 
     public TMP_Text scoreText;
     // Start is called before the first frame update
@@ -31,12 +37,14 @@ public class pizzaActivation : MonoBehaviour
         if(holdingPizza){
             pizza.SetActive(true);
         }
+            
         else{
             pizza.SetActive(false);
         }
 
         if(target == -1){
             arrow.SetActive(false);
+            bonus = 0;
         }
 
     }
@@ -46,12 +54,22 @@ public class pizzaActivation : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         //Check to see if the tag on the collider is equal to Enemy
+        if(other.tag == "pepperoni"){
+            bonus += 0.5;
+            other.gameObject.SetActive(false);
+        }
         if (other.tag == "pizzaShop")
         {
             if(!holdingPizza){
                 holdingPizza = true;
                 arrow.SetActive(true);
                 target = Random.Range(1, 6);
+                if(target != -1){
+                    pep1.SetActive(true);
+                    pep2.SetActive(true);
+                    pep3.SetActive(true);
+                    pep4.SetActive(true);
+                }
                 if(target == 1){
                     arrow.transform.position = house1.transform.position;
                 }
@@ -66,37 +84,38 @@ public class pizzaActivation : MonoBehaviour
                 }
                 else if(target == 5){
                     arrow.transform.position = house5.transform.position;
+                    }
                 }
             }
-        }
+        
        if (other.tag == "House"){
             if(other.name == "house5" && target == 5){
                 holdingPizza = false;
-                score += 2.25;
+                score += (2.25 + bonus);
                 scoreText.text = "Tips: $" + score;
                 target = -1;
             }
             if(other.name == "house1" && target == 1){
                 holdingPizza = false;
-                score += 2.25;
+                score += (2.25 + bonus);
                 scoreText.text = "Tips: $" + score;
                 target = -1;
             }
             if(other.name == "house2" && target == 2){
                 holdingPizza = false;
-                score += 2.25;
+                score += (2.25 + bonus);
                 scoreText.text = "Tips: $" + score;
                 target = -1;
             }
             if(other.name == "house3" && target == 3){
                 holdingPizza = false;
-                score += 2.25;
+                score += (2.25 + bonus);
                 scoreText.text = "Tips: $" + score;
                 target = -1;
             }
             if(other.name == "house4" && target == 4){
                 holdingPizza = false;
-                score += 2.25;
+                score += (2.25 + bonus);
                 scoreText.text = "Tips: $" + score;
                 target = -1;
             }
