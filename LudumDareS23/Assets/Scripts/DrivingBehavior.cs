d736e53c65093c10eed33e0a6ac9e8c1c27960ff
Noah Lovelace <ndlovelace13@gmail.com>
2023-04-29 14:30:47 -0700

    Ai Changes

--- LudumDareS23/Assets/Scripts/DrivingBehavior.cs
+++ LudumDareS23/Assets/Scripts/DrivingBehavior.cs
@@ -8,6 +8,8 @@ public class DrivingBehavior : MonoBehaviour
     [SerializeField]PathCreator pathCreator;
     [SerializeField] float speed = 5;
     float distanceTravelled;
+    //[SerializeField] int direction;
+    Quaternion directionFaced;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +19,12 @@ public class DrivingBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distanceTravelled += speed * Time.deltaTime;
+        distanceTravelled += speed * Time.deltaTime; //* direction;
         transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
         transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+        /*if (direction < 0)
+            transform.rotation = Quaternion.Inverse(directionFaced);
+        else
+            transform.rotation = directionFaced;*/
     }
 }
